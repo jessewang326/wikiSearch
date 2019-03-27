b780d8b9ba4b450f5e37536fe1025741a01b4dbb
Jiaxi Wang <jwang724@uwo.ca>
2019-03-26 21:31:40 -0400

    fix properties

--- src/main/java/com/demo/wysdom/ai/controller/HomeController.java
+++ src/main/java/com/demo/wysdom/ai/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping("home")
+	@RequestMapping({"/", "home"})
 	public String home() {
-		return "home.jsp";
+		return "home";
 	}
 }
